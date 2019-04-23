c52e6af4a9efc17b5ea2dfce54a0b38afacb85f0
MisterTikva <killtank1174@gmail.com>
2019-04-23 09:19:36 +0300

    Add files via upload

---
+++ Sum.py
@@ -0,0 +1,6 @@
+input = open('input.txt');
+output = open('output.txt' , 'w');
+tokens = input.readline().split(' ');
+input.close();
+output.write(str(int(tokens[0]) + int(tokens[1])));
+output.close();
